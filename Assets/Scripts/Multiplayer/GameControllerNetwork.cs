6efaf393fc88db37b313b6972dfc3eb7fa55863c
areena <areena.rehman@funavry.com>
2024-05-10 15:36:47 +0500

    Error-Resolved

--- Assets/Scripts/Multiplayer/GameControllerNetwork.cs
+++ Assets/Scripts/Multiplayer/GameControllerNetwork.cs
@@ -17,11 +17,6 @@ namespace BackgammonNet.Core
 
     public class GameControllerNetwork : MonoBehaviour
     {
-        public HorizontalLayoutGroup topMenu;
-        public List<GameObject> buttons;
-        public bool menuToggle;
-
-
         private PhotonView _photonView;
 
 
@@ -624,64 +619,6 @@ namespace BackgammonNet.Core
             SceneManager.LoadScene(0);
         }
 
-
-        public void MenuButtonToggle()
-        {
-            
-            if (menuToggle)
-            {
-                menuToggle = false;
-
-                StopAllCoroutines();
-                StartCoroutine(AnimateTopMenu(false));
-            }
-            else
-            {
-                menuToggle = true;
-                foreach (var button in buttons)
-                {
-                    button.SetActive(true);
-                }
-                StopAllCoroutines();
-                StartCoroutine(AnimateTopMenu(true));
-            }
-        }
-
-        private IEnumerator AnimateTopMenu(bool toggle)
-        {
-            float elapsedTime = 0;
-            float percentageComplete = 0;
-
-            if (toggle)
-            {
-                while (topMenu.spacing < -270f)
-                {
-                    elapsedTime += Time.deltaTime;
-                    percentageComplete = elapsedTime / 1.8f;
-
-                    topMenu.spacing = Mathf.Lerp(topMenu.spacing, 10f, percentageComplete);
-
-                    yield return new WaitForFixedUpdate();
-                }
-            }
-            else
-            {
-                while (topMenu.spacing > -580f)
-                {
-                    elapsedTime += Time.deltaTime;
-                    percentageComplete = elapsedTime / 1.8f;
-
-                    topMenu.spacing = Mathf.Lerp(topMenu.spacing, -580f, percentageComplete);
-
-                    yield return new WaitForFixedUpdate();
-                }
-
-                foreach (var button in buttons)
-                {
-                    button.SetActive(false);
-                }
-            }
-        }
 
 
 
